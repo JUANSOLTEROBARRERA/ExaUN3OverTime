bfdcb57a1acfa060e6dbbb7e833340bf57cfceed
axlopezre <83840788+axlopezre@users.noreply.github.com>
2022-11-17 21:51:47 -0700

    problema con calendar

--- src/app/new-reservation/new-reservation.page.ts
+++ src/app/new-reservation/new-reservation.page.ts
@@ -19,6 +19,12 @@ export class NewReservationPage implements OnInit {
   public tomorrow: string;
   public rooms: Room[];
   public rol: boolean;
+  
+  //Datos para mandar nuevo guest
+  public name: string;
+  public phone: string;
+  public f_arrival2: string;
+  //public fecha2: Date = new Date();
 
   constructor(private guestService: GuestService, private fb: FormBuilder, private router: Router) {
     let str1 = this.guestService.currentUser();
@@ -40,6 +46,12 @@ export class NewReservationPage implements OnInit {
 
   public padTo2Digits(num: number) {
     return num.toString().padStart(2, '0');
+  }
+
+  public newGuest():void{
+    //console.log(this.name = this.myForm.get('name').value);
+    //console.log(this.phone = this.myForm.get('phone').value);
+    console.log(this.formatDate(this.name = this.myForm.get('fecha2').value));
   }
 
   public formatDate(date: Date) {
@@ -67,7 +79,8 @@ export class NewReservationPage implements OnInit {
       {
         name: ["", Validators.compose([Validators.required])],
         phone: ["", Validators.compose([Validators.required, Validators.minLength(11), Validators.pattern('^[\+][(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')])],
-        room: ["", Validators.compose([Validators.required])]
+        room: ["", Validators.compose([Validators.required])],
+        fecha2: ["", Validators.compose([Validators.required])]
       }
     );
 
@@ -81,6 +94,9 @@ export class NewReservationPage implements OnInit {
         { type: 'pattern', message: "EL teléfono está mal formado" }
       ],
       room: [
+        { type: 'required', message: "La habitación es obligatoria." }
+      ],
+      fecha2: [
         { type: 'required', message: "La habitación es obligatoria." }
       ]
     }
