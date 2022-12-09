86a48ee856fdc8c5f71b4a4e4297615f0bce2d61
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2022-12-09 16:11:49 -0700

    jpeg

--- src/app/new-reservation/new-reservation.page.ts
+++ src/app/new-reservation/new-reservation.page.ts
@@ -839,7 +839,7 @@ export class NewReservationPage implements OnInit {
       let dividir3 = dividir2[5].split('.');
       // dividir3[0]=nombre de imagen; dividir3[1]=extension del archivo
 
-      let fileObject = new File([this.imgURL], this.imageName()+"", { type: "image/jpg"});
+      let fileObject = new File([this.imgURL], this.imageName()+"", { type: "image/jpeg"});
       this.myForm.controls.name.setValue("hola2")
       //this.myForm.controls.name.setValue("URL:"+this.imgURL+"TIPO:"+"image/"+dividir3[1])
       this.guestService.storeImage2(fileObject)
