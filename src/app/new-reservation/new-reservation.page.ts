bf60d54d4c022d444c78ec06b6992c0c00a60b86
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2022-12-09 20:18:30 -0700

    con base 64

--- src/app/new-reservation/new-reservation.page.ts
+++ src/app/new-reservation/new-reservation.page.ts
@@ -843,7 +843,7 @@ export class NewReservationPage implements OnInit {
     }
     ).then((res) => {
       //this.imgURL = res;
-      this.imgURL = this.WebView.convertFileSrc(res);
+      this.imgURL = this.WebView.convertFileSrc('data:image/jpeg;base64,' + res);
       this.myForm.controls.name.setValue(this.imgURL)
       //Ejemplo: file:///data/user/0/io.ionic.starter/cache/1670619070156.jpg
       //let dividir = this.imgURL.split('///');
